61eb515ba4c5dae7dd84b5c13acc3ba2e2128904
MysticfalconVT <rboskind@gmail.com>
2020-12-31 20:13:53 -0500

    adding PWA stuff

--- BoskindGatsby/gatsby-config.js
+++ BoskindGatsby/gatsby-config.js
@@ -22,5 +22,23 @@ export default {
         token: process.env.SANITY_TOKEN,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `static/logo.svg`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/blog/`, `/units/*`, `/links/`],
+      },
+    },
   ],
 };
