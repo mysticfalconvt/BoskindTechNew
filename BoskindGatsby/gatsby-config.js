3c1d313b17101e457fc4b2696213e84ba8e489c1
MysticfalconVT <rboskind@gmail.com>
2021-01-13 19:28:18 -0500

    trying to get analytincs working

--- BoskindGatsby/gatsby-config.js
+++ BoskindGatsby/gatsby-config.js
@@ -11,11 +11,22 @@ export default {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      trackingId: 'UA-31032206-2',
-      head: true,
-      anonymize: true,
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-31032206-2', // Google Analytics / GA
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
     },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   trackingId: 'UA-31032206-2',
+    //   head: true,
+    //   anonymize: true,
+    // },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     `gatsby-plugin-transition-link`,
