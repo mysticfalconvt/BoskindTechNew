d11da5f0f062ce4c49284ca59407292e12c1a14b
mysticfalconvt <rboskind@gmail.com>
2021-03-27 11:48:45 -0400

    added portflio basic

---
+++ BoskindGatsby/src/pages/portfolio.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const GridContainer = styled.section`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  div {
+    background: lightgray;
+    padding: 10px;
+    border-radius: 10px;
+    margin: 10px;
+  }
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  padding: 10px;
+  flex-wrap: wrap;
+
+  p::before {
+    content: '·';
+    padding: 5px;
+  }
+`;
+
+export default function portfolio() {
+  return (
+    <main>
+      <h1 className="center">Rob Boskind - Web Developer</h1>
+      <h3 className="center">Looking to work for you</h3>
+      <GridContainer>
+        <div>
+          <h3 className="center">About Me</h3>
+          <p>
+            I am looking to make peoples lives better with code. What can I
+            bring to your company? A love for code, and a love for data
+            organization. I am a math teacher, but I have been a software
+            developer as well for many years. I want to make the transition into
+            full time development.
+          </p>
+          <p>
+            <a href="https://school-dashboard-demo.herokuapp.com/">
+              School Dashboard
+            </a>
+            - This is a dashboard my school has used to coordinate many school
+            functions. It was created using Node and Express with a MongoDB
+            backend and Pug for templating. This demo allows full use, but the
+            data is much better in a school envornment with hundreds of teachers
+            and students as users.
+          </p>
+        </div>
+        <div>
+          <h3 className="center">Technologies</h3>
+          <FlexContainer>
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>JavaScript</p>
+            <p>React</p>
+            <p>MongoDB</p>
+            <p>NodeJS</p>
+            <p>Express</p>
+            <p>Gatsby</p>
+            <p>Next JS</p>
+          </FlexContainer>
+        </div>
+      </GridContainer>
+    </main>
+  );
+}
