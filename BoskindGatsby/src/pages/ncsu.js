1e44bad1ecebd6deea42518a991df95dac0a451c
MysticfalconVT <rboskind@gmail.com>
2021-04-14 07:04:07 -0400

    working on program for JC

---
+++ BoskindGatsby/src/pages/ncsu.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ColumnsStyles = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-gap: 100px;
+`;
+
+export default function schoolIdeas() {
+  return (
+    <div>
+      <h1 className="center">NCSU Benefits</h1>
+      <ColumnsStyles>
+        <div>
+          <p>
+            In my time at NCUJHS I have become more and more focused on
+            supporting the technology of the school. As the staff of the school
+            learned of my skills they have relied more and more on my abilities
+            to improve their effectiveness through technology.
+          </p>
+          <p>One of my most useful skills is software engineering. </p>
+        </div>
+        <div>
+          <h3>Specific applications</h3>
+          <ul>
+            <li>School Dashboard</li>
+          </ul>
+        </div>
+      </ColumnsStyles>
+    </div>
+  );
+}
